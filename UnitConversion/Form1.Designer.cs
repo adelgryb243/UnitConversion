18cd883aa807cd372d9ea2be3f58319f803f23b1
Adele Gryba <adelgryb243@ed.amdsb.ca>
2020-04-26 23:25:53 -0400

    Add project files.

---
+++ UnitConversion/Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace UnitConversion
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.convertButton = new System.Windows.Forms.Button();
+            this.optionBox = new System.Windows.Forms.TextBox();
+            this.valueBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // convertButton
+            // 
+            this.convertButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.convertButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.convertButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.convertButton.Location = new System.Drawing.Point(66, 267);
+            this.convertButton.Name = "convertButton";
+            this.convertButton.Size = new System.Drawing.Size(256, 117);
+            this.convertButton.TabIndex = 0;
+            this.convertButton.Text = "Convert!";
+            this.convertButton.UseVisualStyleBackColor = true;
+            this.convertButton.Click += new System.EventHandler(this.convertButton_Click);
+            // 
+            // optionBox
+            // 
+            this.optionBox.Location = new System.Drawing.Point(553, 114);
+            this.optionBox.Name = "optionBox";
+            this.optionBox.Size = new System.Drawing.Size(60, 20);
+            this.optionBox.TabIndex = 1;
+            // 
+            // valueBox
+            // 
+            this.valueBox.Location = new System.Drawing.Point(553, 153);
+            this.valueBox.Name = "valueBox";
+            this.valueBox.Size = new System.Drawing.Size(60, 20);
+            this.valueBox.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(297, 150);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(235, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Enter value to be converted:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(268, 111);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(264, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Choose conversion option (1-4):";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 27.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(112, 19);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(306, 43);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Linear Conversion";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(23, 80);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(192, 18);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "1. Inches to Centimetres";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(23, 113);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(176, 18);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "2. Feet to Centimetres";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(23, 151);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(147, 18);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "3. Yards to Metres";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(23, 194);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(173, 18);
+            this.label7.TabIndex = 9;
+            this.label7.Text = "4. Miles to Kilometres";
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outputLabel.ForeColor = System.Drawing.Color.ForestGreen;
+            this.outputLabel.Location = new System.Drawing.Point(391, 267);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(369, 117);
+            this.outputLabel.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Bisque;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.outputLabel);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.valueBox);
+            this.Controls.Add(this.optionBox);
+            this.Controls.Add(this.convertButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button convertButton;
+        private System.Windows.Forms.TextBox optionBox;
+        private System.Windows.Forms.TextBox valueBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label outputLabel;
+    }
+}
+
---
+++ UnitConversion/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UnitConversion
+{
+    public partial class Form1 : Form
+    {
+        int option;
+        int value;
+        public Form1()
+        {
+            InitializeComponent();      
+        }
+      
+        public void convertButton_Click(object sender, EventArgs e)
+        {
+            option = Convert.ToInt32(optionBox.Text);
+            value = Convert.ToInt32(valueBox.Text);
+            if (option == 1)
+            {
+                double final = InchesToCm(value);
+                outputLabel.Text = value + " Inches  = " + final.ToString("0.00") + " Centimetres";
+            }
+            else if (option == 2)
+            {
+                double final = FeetToCm(value);
+                outputLabel.Text = value + " Feet  = " + final.ToString("0.00") + " Centimetres";
+            }
+            else if (option == 3)
+            {
+                double final = YardsToMetres(value);
+                outputLabel.Text = value + " Yards  = " + final.ToString("0.00") + " Metres";
+            }
+            else if (option == 4)
+            {
+                double final = MilesToKm(value);
+                outputLabel.Text = value + " Miles  = " + final.ToString("0.00") + " Kilometres";
+            }
+        }
+        /// <summary>
+        /// converts inches to centimetres
+        /// </summary>
+        /// <param name="val">the value inputted to be converted</param>
+        /// <returns>product of calculation</returns>
+        public static double InchesToCm(int val)
+        {
+            double conversion = 0.00;
+            conversion = val * 2.54;
+            return conversion;
+
+        }
+        /// <summary>
+        /// converts feet to centimetres
+        /// </summary>
+        /// <param name="val">the value inputted to be converted</param>
+        /// <returns>product of calculation</returns>
+        public static double FeetToCm(int val)
+        {
+            double conversion = 0.00;
+            conversion = val * 30.48;
+            return conversion;
+        }
+        /// <summary>
+        /// converts yards to metres
+        /// </summary>
+        /// <param name="val">the value inputted to be converted</param>
+        /// <returns>product of calculation</returns>
+        public static double YardsToMetres(int val)
+        {
+            double conversion = 0.00;
+            conversion = val * 0.91;
+            return conversion;
+        }
+        /// <summary>
+        /// converts miles to kilometeres
+        /// </summary>
+        /// <param name="val">the value inputted to be converted</param>
+        /// <returns>product of calculation</returns>
+        public static double MilesToKm(int val)
+        {
+            double conversion = 0.00;
+            conversion = val * 1.6;
+            return conversion;
+        }
+
+    }
+}
